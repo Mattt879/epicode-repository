a8cfdd771389f2a409d8ce8e0196d0893b947339
mattt92 <cellimatteo8@gmail.com>
2025-12-06 12:26:38 +0100

    esercizi

---
+++ esercizi_variabili.py
@@ -0,0 +1,10 @@
+#crea una variabile con il tuo nome e stampala
+nome = "matteo"
+print (nome)
+#chiedi all'utente quanti anni ha e 
+eta = int(input("quanti anni hai? "))
+print("la tua età è:", 2 * eta)
+#crea 2 numeri e stampa la loro somma
+a = 1 
+b= 10
+print (a+b)
